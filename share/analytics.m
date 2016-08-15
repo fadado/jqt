b89b6934b71c53f8b66d6c6373b113bd505315b9
Joan Josep Ordinas Rosa <jordinas@gmail.com>
2016-08-15 08:55:43 +0200

    Added analytics

--- share/analytics.m
+++ share/analytics.m
@@ -1,8 +1,17 @@
 <#
- # Usage: <%partial analytics UA-8673551-2>
+ # Usage: <%partial analytics UA-82432866-1>
  #
  # $1: analytics UA
  #>
+<script type="text/javascript">
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  ga('create', '$1', 'auto');
+  ga('send', 'pageview');
+</script>
+<# OLD VERSION
 <script type="text/javascript">
     var _gaq = _gaq || [];
     _gaq.push(["_setAccount", "$1"]);
@@ -16,6 +25,19 @@
         s.parentNode.insertBefore(ga, s);
     })();
 </script>
+#>
+<# LAST VERSION
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-82432866-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+#>
 <#
 vim:ts=4:sw=4:ai:et:fileencoding=utf8:syntax=html
 #>
