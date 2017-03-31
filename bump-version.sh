fe778b928d15ee1f927b5afb1adda23d4b5c637f
Joan Josep Ordinas Rosa <jordinas@gmail.com>
2017-03-31 08:55:06 +0200

    Version bump to 0.5.0

--- bump-version.sh
+++ bump-version.sh
@@ -35,8 +35,8 @@ if [[ -f VERSION ]]; then
     cp /tmp/$$-changes CHANGES
     rm /tmp/$$-changes
     git add CHANGES VERSION
-    sed -i "s/^declare -r VERSION=/&'$NEXT_VERSION'"
-    git commit -m "Version bump to $NEXT_VERSION"
+    sed -i "s/^declare -r VERSION=/&'$NEXT_VERSION'/" bin/jqt
+    git commit -am "Version bump to $NEXT_VERSION"
     git tag -a -m "Tagging version $NEXT_VERSION" "v$NEXT_VERSION"
     [[ $PUSH == yes ]] && git push origin --tags
 else
